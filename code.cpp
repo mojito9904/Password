670f0f2142635da6cce803425b1707bc98cfbbae
Mojito <106539365+mojito9904@users.noreply.github.com>
2024-09-28 17:20:58 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,80 +1,63 @@
-#include <iostream>
-#include <string>
-#include <cstdlib> 
-#include <ctime>  
-#include <cctype>
-#include <algorithm>
+#include <iostream>    
+#include <string>           // Libreria per le stringhe 
+#include <cstdlib>          // Per usare il rand
+#include <ctime>            // Per usare il time(0)
+#include <cctype>           // Per rendere tutto maiuscolo un carattere 
+#include <algorithm>        // Per usare il transform 
+
 using namespace std;
 
-//prototipi
-bool continuare (string& risposta);
+// Prototipi
+bool continuare(string& risposta);  //prototipo per decidere se continuare oppure no 
 
 int main() {
     srand(time(0));  
-    int lunghezza_password=10;
-    int numeri[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    char lettere_maiuscole[26] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
-    char lettere_minuscole[26] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-    char caratteri[7] = {'!', '?', '@', '#', '$', '%', '&'};
-    bool ciclo=true;
+    
+    // Array unificato con tutti gli elementi a disposizione 
+    string caratteri = "0123456789"
+                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                       "abcdefghijklmnopqrstuvwxyz"
+                       "!@#$%&?";
+    
+    int lunghezza_password = 10;  //la lunghezza che deve essere la nostra password
+    bool ciclo = true;            //variabile booolena per la gestione del ciclo 
+    
     while (ciclo) { 
-        cout<<"Generazione delle prime 10 password"<<endl; 
-        for (int i=0;i<10;i++){
-            string password = ""; 
-            for (int i = 0; i < lunghezza_password; i++) {  
-            int scelta = (rand() % 4) + 1; 
-            switch (scelta) {
-                case 1: {
-                    int index = rand() % 10;  // Indice per i numeri (0-9)
-                    //cout << "caso 1: " << numeri[index] << endl;
-                    password += to_string(numeri[index]);  
-                    break;
-                }
-                case 2: {
-                    int index = rand() % 26;  // Indice per lettere maiuscole
-                    //cout << "caso 2: " << lettere_maiuscole[index] << endl;
-                    password += lettere_maiuscole[index];
-                    break;
-                }
-                case 3: {
-                    int index = rand() % 26;  // Indice per lettere minuscole
-                    //cout << "caso 3: " << lettere_minuscole[index] << endl;
-                    password += lettere_minuscole[index];
-                    break;
-                }
-                case 4: {
-                    int index = rand() % 7;  // Indice per i caratteri speciali
-                    //cout << "caso 4: " << caratteri[index] << endl;
-                    password += caratteri[index];
-                    break;
-                }
-                default:
-                    break;
+        cout << "Generazione delle prime 10 password" << endl; 
+        
+        for (int i = 0; i < 10; i++) {              //ciclo che viene eseguito per 10 volte 
+            string password = "";                   //dichiarazizone della stringa password che è vuota 
+            
+            for (int j = 0; j < lunghezza_password; j++) {  //ciclo che viene eseguito per la creazione della password
+                int index = rand() % caratteri.length();    // Scelta casuale dalla stringa caratteri 
+                password += caratteri[index];               // Aggiunge il carattere casuale alla password
             }
-        }
-        cout<<"["<<i+1<<"]"<<"password generata :"<<password<<endl;
+            
+            cout << "[" << i + 1 << "] password generata: " << password << endl;        //stampa le varie password 
         }
         
-        cout<<"vuoi continuare con altre password: [si] o [no]";
+        // Richiedi se continuare
+        cout << "Vuoi continuare con altre password: [si] o [no] ";
         string risposta;
-        cin>>risposta;
-        ciclo=continuare(risposta);
+        cin >> risposta;
+        ciclo = continuare(risposta);
     }
+    
     return 0;
 }
-//funzione
 
-bool continuare(string& risposta){
-    
-    transform(risposta.begin(), risposta.end(), risposta.begin(),[](unsigned char c) { return toupper(c); });
+// Funzione per gestire la scelta di continuare
+bool continuare(string& risposta) {
+    transform(risposta.begin(), risposta.end(), risposta.begin(), [](unsigned char c) { return toupper(c); }); //permette di trasformare la stringa della risposta in maiuscolo 
 
     if (risposta == "NO") {
-        return false;
+        return false;               //se la rispsota è uguale a NO allora la funzione fa un return di una variabile booleana su falso 
     } else if (risposta == "SI") {
-        return true;
+        return true;                //se invece la risposta è uguale a SI allora la funzione restituisce una variabile booleana su vero 
     } else {
-        cout << "Risposta sbagliata, si prega di rinserire la scelta [si] o [no]: " << endl;
-        string nuovaRisposta;
+        //altrimenti si fa una chiamata ricorsiva della stessa funzione fino a quando non si effettua una scelta giusta 
+        cout << "Risposta sbagliata, si prega di reinserire la scelta [si] o [no]: " << endl;
+        string nuovaRisposta;                               
         cin >> nuovaRisposta;
         return continuare(nuovaRisposta); 
     }
